2ab5fb25b25c12611c88c81fe553ffb6cdf5abc2
bo <389616868@qq.com>
2017-12-13 18:44:49 +0800

    12_13

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
-/**
- * Created by geely
- */
+
 @Controller
 @RequestMapping("/cart/")
 public class CartController {
